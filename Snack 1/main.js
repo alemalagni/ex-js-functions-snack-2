eaa86e87945b42c5e495df22ed56b8c17f2111dd
alemalagni <alemalagni@gmail.com>
2025-09-02 12:42:07 +0200

    fea: add Arrow Function

--- Snack 1/main.js
+++ Snack 1/main.js
@@ -1,7 +1,11 @@
-function somma(a, b) {
+function SommaDichiarativa(a, b) {
     console.log(a + b);
 };
 
 const SommaAnonima = function (a, b) {
     console.log(a + b);
 };
+
+SommaArrow((a, b) => {
+    console.log(a + b);
+});
