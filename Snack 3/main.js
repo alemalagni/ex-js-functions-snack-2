4db4013b8fd7918528f3d5db8f463ef44a75d22b
alemalagni <alemalagni@gmail.com>
2025-09-02 12:51:56 +0200

    feat: add script

--- Snack 3/main.js
+++ Snack 3/main.js
@@ -2,3 +2,6 @@ const somma = (a, b) => { a + b };
 const moltiplica = (a, b) => { a * b };
 
 const EseguiOperazione = (a, b, operazione) => { operazione(a, b) };
+
+console.log(EseguiOperazione(2, 3, somma()));
+console.log(EseguiOperazione(2, 3, moltiplica()));
